dc287edef6916904404132a38814372ab5c38919
AyaExpTech <ayasakaexpresstech@gmail.com>
2023-02-07 14:31:32 +0900

    たぶんできた

--- script.js
+++ script.js
@@ -1,54 +1,109 @@
 /**
- * ページロード時、history配列を定義
- * ON-OFF履歴の保存に使います
+ * 方針
+ * - ログデータはすべてcookieの"log"に投げてなんとかします
+ *     - cookieのログには、base64(UTF-8)エンコードしたJSON文字列を保存します。
+ *     - JSONが示すオブジェクトは`[unixTime(Number), status(Boolean)]`をまとめた二次元配列です。
+ * - ページロード時、該当cookieが存在しない場合は"1970年1月3日00:00(UTC)にOFF"を加えます(init)
+ * - Startボタン押下時、直近のログがOFFなら"現在時刻にON"を加えます(start)
+ * - Stopボタン押下時、直近のログがONなら"現在時刻にOFF"を加えます(stop)
+ * - Resetボタン押下時、一度すべてのログを削除してから"1970年1月3日00:00(UTC)にOFF"を加えます(reset)
+ * - ページロード時、0.5秒間隔で"ログとinputを監査して#counterの表示を更新する関数(update)"を実行します。
  */
 
-const history = [];
+/**
+ * base64toObj(base) - base64→オブジェクトに変換します。マルチバイト非対応。
+ * @param {String} base 
+ * @returns {*} - 変換後のオブジェクト。ObjectとかArrayとか？
+ */
+const base64toObj = (base) => JSON.parse(atob(base));
 
 /**
- * Startボタンが押されたとき、履歴にUNIXタイムをぶん投げる
+ * objToBase64(obj) - オブジェクト→base64に変換します。マルチバイト非対応。
+ * @param {*} obj - 変換するオブジェクト
+ * @returns {String} - base64エンコードしたオブジェクトのJSON文字列
+ */
+const objToBase64 = (obj) => btoa(JSON.stringify(obj));
+
+/**
+ * getCookieByKey(key) - cookieの特定の値だけ引っ張ってきます
+ * @param {*} key - 取得したいkey
+ * @returns - 指定したkeyのvalue。存在しないkeyを参照した場合はundefinedを返します
+ */
+const getCookieByKey = (key) => (document.cookie.match(new RegExp(key + '\=([^\;]*)\;*')) ?? [0, undefined])[1];
+
+/**
+ * init() - ページロード時実行。cookieにlogが存在しなければ"1970年1月1日00:00(UTC)にOFF"を加えます
+ */
+const init = () => {
+    if (getCookieByKey("log") == undefined) {
+        const newObj = [[0, false]];
+        document.cookie = "log=" + objToBase64(newObj) + ";";
+    }
+};
+
+/**
+ * start() - Startボタン押下時実行。直近のログがOFFなら"現在時刻にON"を加えます
  */
 const start = () => {
-    history.at(-1) ? "" : history.push([Date.now(), true]);
-    history.length == 0 ? history.push([Date.now(), true]) : "";
+    const log = base64toObj(getCookieByKey("log"));
+    if (!log.at(-1)[1]) {
+        log.push([Date.now(), true]);
+    }
+    document.cookie = "log=" + objToBase64(log) + ";";
 };
 
 /**
- * Stopボタンが押されたとき、履歴にUNIXタイムをぶん投げる
+ * stop() - Stopボタン押下時実行。直近のログがONなら"現在時刻にOFF"を加えます
  */
 const stop = () => {
-    history.at(-1) ? history.push([Date.now(), false]) : "";
+    const log = base64toObj(getCookieByKey("log"));
+    if (log.at(-1)[1]) {
+        log.push([Date.now(), false]);
+    }
+    document.cookie = "log=" + objToBase64(log) + ";";
 };
 
 /**
- * 一定間隔で実行する関数
- * - 現在の時間-記録制限時間をしてhistoryからそれより前の記録を除外
- * - history[0]がfalse記録なら現在の時間-記録制限時間にtrue記録を捏造
- * - falseなやつ全部足してtrueなやつ全部引く
- * - 整形して#counterに投げ込む
+ * reset() - Resetボタン押下時実行。一度すべてのログを削除してから"1970年1月3日00:00(UTC)にOFF"を加えます
  */
-const interval = () => {
-    const docV = (e) => document.querySelector(e).value;
-    const heldMs = docV("#input-hour") * 3600000 + docV("#input-min") * 60000 + docV("#input-sec") * 1000;
-    const heldUNIX = Date.now() - heldMs;
-    console.log(heldMs);
-    console.log(heldUNIX);
-    console.log(history);
-    const validHistory = [];
-    history.forEach(e => (e[0] > heldUNIX) ? validHistory.push(e) : "");
-    history.length ? (validHistory[0][1] ? "" : validHistory.unshift([heldUNIX, true])) : "";
-    console.log(validHistory);
-    let time = 0;
-    validHistory.forEach(e => e[1] ? (time -= e[0]) : (time += e[0]));
-    console.log(time);
-    const timer = [
-        ("000" + Math.floor(time / 3600000).toString()).slice(-2), //hour
-        ("000" + Math.floor(time / 60000 % 60).toString()).slice(-2), //min
-        ("000" + Math.floor(time / 1000 % 60).toString()).slice(-2), //sec
-        ("000" + Math.floor(time / 10 % 100).toString()).slice(-2) //ms
-    ];
-    console.log(timer);
-    document.querySelector("#counter").innerHTML = `${timer[0]}:${timer[1]}:${timer[2]}.${timer[3]}`;
+const reset = () => {
+    const newObj = [[0, false]];
+    document.cookie = "log=" + objToBase64(newObj) + ";";
 }
 
-setInterval(interval, 1000)
+/**
+ * update() - ページロード時0.5秒間隔で実行。ログとinputを監査して#counterの表示を更新する関数
+ */
+const update = () => {
+    var logArray = base64toObj(getCookieByKey("log"));
+    var nowUnix = Date.now();
+    var recordRange = 0;
+    recordRange += document.querySelector("#input-hour").value * 1000 * 60 * 60;
+    recordRange += document.querySelector("#input-min").value * 1000 * 60;
+    recordRange += document.querySelector("#input-sec").value * 1000;
+    var recordLimit = nowUnix - recordRange;
+    /* 記録制限のタイミング以降のログを制作 */
+    var recordLog = logArray.filter(e => e[0] >= recordLimit);
+    var countTime = 0;
+    recordLog.forEach(function (value) {
+        value[1] ? countTime -= value[0] : countTime += value[0];
+    });
+    /* 最後がONなら今を足す */
+    if (logArray.at(-1)[1]) {
+        countTime += nowUnix;
+    }
+    /* 制限時点でONなら制限開始時刻を引く */
+    var ignored = logArray.length - recordLog.length;
+    if (ignored >= 0 && logArray[ignored - 1][1]) {
+        countTime -= recordLimit;
+    }
+    document.querySelector("#counter").innerHTML = String(new Date(countTime).toISOString().slice(11, 23));
+}
+
+/**
+ * ページロード時に実行するやつ
+ */
+window.addEventListener("load", function () {
+    init();
+    setInterval(update, 1 / 60);
+});
